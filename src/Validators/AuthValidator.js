c9561612ec384711a4588b62d8c4c2c869efbd0e
alexbeldam <alexbeldam@gmail.com>
2025-05-12 21:26:53 -0300

    feat: JWT Token Middleware

--- src/Validators/AuthValidator.js
+++ src/Validators/AuthValidator.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const { z } = require("zod");
 const { validateRequest } = require("zod-express-middleware");
 
--- src/routes.js
+++ src/routes.js
@@ -2,20 +2,23 @@ const { Router } = require("express");
 const UsuariosController = require("./Controllers/UsuariosController");
 const SessoesController = require("./Controllers/SessoesController");
 const AuthController = require("./Controllers/AuthController");
+
 const UsuarioValidator = require("./Validators/UsuarioValidator");
 const SessaoValidator = require("./Validators/SessaoValidator");
 const AuthValidator = require("./Validators/AuthValidator");
 
+const verificarJwt = require("./Middlewares/verificarJwt");
+
 const rotas = Router();
 
 rotas.post("/usuarios", UsuarioValidator.create, UsuariosController.create);
-rotas.get("/usuarios", UsuariosController.read);
-rotas.put("/usuarios/:id", UsuarioValidator.destroy, UsuariosController.update);
-rotas.delete("/usuarios/:id", UsuarioValidator.update, UsuariosController.delete);
+rotas.get("/usuarios", verificarJwt, UsuariosController.read);
+rotas.put("/usuarios/:id", verificarJwt, UsuarioValidator.destroy, UsuariosController.update);
+rotas.delete("/usuarios/:id", verificarJwt, UsuarioValidator.update, UsuariosController.delete);
 
-rotas.post("/sessoes", SessaoValidator.create, SessoesController.create);
-rotas.get("/sessoes", SessoesController.read);
-rotas.delete("/sessoes/:id", SessaoValidator.destroy, SessoesController.delete);
+rotas.post("/sessoes", verificarJwt, SessaoValidator.create, SessoesController.create);
+rotas.get("/sessoes", verificarJwt, SessoesController.read);
+rotas.delete("/sessoes/:id", verificarJwt, SessaoValidator.destroy, SessoesController.delete);
 
 rotas.post("/login", AuthValidator.login, AuthController.login);
 
