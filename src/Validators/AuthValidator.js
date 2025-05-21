859607f1db2409a905935b0d006d9329ae9e286f
alexbeldam <alexbeldam@gmail.com>
2025-05-20 22:40:54 -0300

    fix: Empty strings not allowed

--- src/Validators/AuthValidator.js
+++ src/Validators/AuthValidator.js
@@ -3,8 +3,11 @@ const { validateRequest } = require("zod-express-middleware");
 
 const login = validateRequest({
   body: z.object({
-    email: z.string({ required_error: "O email é obrigatório" }).email("O email é inválido"),
-    senha: z.string({ required_error: "A senha é obrigatória" }),
+    email: z
+      .string({ required_error: "O email é obrigatório" })
+      .min(1, "O email é obrigatório")
+      .email("O email é inválido"),
+    senha: z.string({ required_error: "A senha é obrigatória" }).min(1, "A senha é obrigatória"),
   }),
 });
 
--- src/Validators/UsuarioValidator.js
+++ src/Validators/UsuarioValidator.js
@@ -4,10 +4,13 @@ const { validateRequest } = require("zod-express-middleware");
 
 const create = validateRequest({
   body: z.object({
-    nome: z.string({ required_error: "O nome é obrigatório" }),
-    email: z.string({ required_error: "O email é obrigatório" }).email("O email é inválido"),
-    senha: z.string({ required_error: "A senha é obrigatória" }),
-    cargo: z.string({ required_error: "O cargo é obrigatório" }),
+    nome: z.string({ required_error: "O nome é obrigatório" }).min(1, "O nome é obrigatório"),
+    email: z
+      .string({ required_error: "O email é obrigatório" })
+      .min(1, "O email é obrigatório")
+      .email("O email é inválido"),
+    senha: z.string({ required_error: "A senha é obrigatória" }).min(1, "A senha é obrigatória"),
+    cargo: z.string({ required_error: "O cargo é obrigatório" }).min(1, "O cargo é obrigatório"),
   }),
 });
 
