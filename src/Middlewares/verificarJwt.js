dd60782f6f33fa04f11005ab12b12385d12db96b
alexbeldam <alexbeldam@gmail.com>
2025-05-26 12:39:42 -0300

    fix: can't disassemble undefined

--- src/Middlewares/verificarJwt.js
+++ src/Middlewares/verificarJwt.js
@@ -12,8 +12,12 @@ function verificarJwt(req, res, next) {
 
   if (!token) return res.status(403).json({ message: "JWT token não encontrado" });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, { usuario }) => {
-    if (err) return res.status(403).json({ message: "JWT token inválido" });
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "JWT token inválido" });
+    }
+
+    const { usuario } = decoded;
 
     req.usuarioId = usuario._id;
     req.permissao = usuario.permissao;
